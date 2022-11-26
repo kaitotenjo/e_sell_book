928aa264b3800f4c30a4d87387e84b38c4329308
kaito tenjo <toiyeuranmori@gmail.com>
2022-11-26 19:01:52 +0700

    product site , product n-n category

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@ import "bootstrap";
 import "header";
 import "owl.carousel";
 import "@fortawesome/fontawesome-free";
+import { $, jQuery } from 'jquery';
+window.$ = $
+window.jQuery = jQuery
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
-99.times do |n|
-name = Faker::Name.name
-email = "abc#{n+1}@gmail.com"
-password = "password"
-User.create!(name: name,
-    email: email,
-    password:password,
-    password_confirmation: password,
-    )
+Category.create!(name: "action")
+Category.create!(name:" romantic")
+
+category= Category.first
+
+50.times do |n|
+name = "dragon ball tap#{n+1}"
+price = "17500"
+discription=" khong biet viet gi cung duoc ha"
+amount= n+1
+product = Product.create(
+    name:  name,
+    price: price,
+    discription: discription,
+    amount: amount
+)
+ProductCategory.create(product: product , category: category)
 end
